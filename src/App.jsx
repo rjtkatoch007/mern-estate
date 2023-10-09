0adda2082c6de6fbbd7cfe589a66d48d050946e0
rajat <rajat.k@sphinxworldbiz.co>
2023-10-09 18:40:08 +0530

    Installed react and tailwindcss and configured.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-red-500'>Hello Rajat Katoch</h1>
+
+    </>
+  )
+}
+
+export default App
